893f92748ec527e07c2f2e469a5b0916a0fb2477
Felicitylnf <675211848@qq.com>
2018-12-23 15:48:50 +0800

    update config

--- components/MapSection/mapConfig.js
+++ components/MapSection/mapConfig.js
@@ -26,39 +26,82 @@ export default () => [
     ]
   },
   {
-    //
+    //天龙八部
     id: 2,
     book_url:
-      'https://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E5%80%9A%E5%A4%A9.png',
+      'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E5%A4%A9%E9%BE%99%E5%85%AB%E9%83%A8.png',
     point: [
       {
-        //吐蕃
-        left: '10px',
-        top: '200px',
-        x: true,
-        desc: ''
+        //吐蕃大雪山
+        left: '328px',
+        top: '748px',
+        x: false,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E5%90%90%E8%95%83%E5%A4%A7%E9%9B%AA%E5%B1%B1.png',
       },
       {
-        //吐蕃
-        left: '10px',
-        top: '200px',
-        x: true,
-        desc: ''
+        //西夏一品堂
+        left: '684px',
+        top: '594px',
+        x: false,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E8%A5%BF%E5%A4%8F%E4%B8%80%E5%93%81%E5%A0%82.png',
       },
       {
-        //西夏
-        left: '10px',
-        top: '200px',
-        x: true,
-        desc: ''
+        //新疆天山
+        left: '151px',
+        top: '416px',
+        x: false,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E6%96%B0%E7%96%86%E5%A4%A9%E5%B1%B1.png',
       },
       {
-        //西夏
-        left: '10px',
-        top: '200px',
-        x: true,
-        desc: ''
+        //青海星宿派
+        left: '517px',
+        top: '685px',
+        x: false,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E9%9D%92%E6%B5%B7%E6%98%9F%E5%AE%BF%E6%B4%BE.png',
       }
+      {
+        //丐帮
+        left: '821px',
+        top: '699px',
+        x: false,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E4%B8%90%E5%B8%AE.png',
+      }
+      {
+        //河南
+        left: '883px',
+        top: '740px',
+        x: true,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E6%B2%B3%E5%8D%97.png',
+      }
+      {
+        //雁门关
+        left: '842px',
+        top: '580px',
+        x: true,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E9%9B%81%E9%97%A8%E5%85%B3.png',
+      }
+      {
+        //云南大理
+        left: '534px',
+        top: '928px',
+        x: false,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E4%BA%91%E5%8D%97%E5%A4%A7%E7%90%86.png',
+      }
+      {
+        //江苏无锡
+        left: '1016px',
+        top: '766px',
+        x: true,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E6%97%A0%E9%94%A1.png',
+      }
+      {
+        //山东泰山
+        left: '1300px',
+        top: '1463px',
+        x: false,
+        desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E5%B1%B1%E4%B8%9C%E6%B3%B0%E5%B1%B1.png',
+      }
+      
     ]
   },
   {
