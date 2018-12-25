bd84c375040f5f1c1399fe9cb14362aaee8c0c21
Felicitylnf <675211848@qq.com>
2018-12-25 10:32:11 +0800

    update  map point

--- components/MapSection/mapConfig.js
+++ components/MapSection/mapConfig.js
@@ -19,7 +19,7 @@ export default () => [
       {
         //浙江绍兴
         left: '1022px',
-        top: '765px',
+        top: '833px',
         x: true,
         desc: 'https://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/yuenvjian/%E6%B5%99%E6%B1%9F%E7%BB%8D%E5%85%B4.png',
       },
@@ -58,42 +58,42 @@ export default () => [
         top: '685px',
         x: false,
         desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E9%9D%92%E6%B5%B7%E6%98%9F%E5%AE%BF%E6%B4%BE.png',
-      }
+      },
       {
         //丐帮
         left: '821px',
         top: '699px',
         x: false,
         desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E4%B8%90%E5%B8%AE.png',
-      }
+      },
       {
         //河南
         left: '883px',
         top: '740px',
         x: true,
         desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E6%B2%B3%E5%8D%97.png',
-      }
+      },
       {
         //雁门关
         left: '842px',
         top: '580px',
         x: true,
         desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E9%9B%81%E9%97%A8%E5%85%B3.png',
-      }
+      },
       {
         //云南大理
         left: '534px',
         top: '928px',
         x: false,
         desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E4%BA%91%E5%8D%97%E5%A4%A7%E7%90%86.png',
-      }
+      },
       {
         //江苏无锡
         left: '1016px',
         top: '766px',
         x: true,
         desc: 'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap1/tianlongbabu/%E6%97%A0%E9%94%A1.png',
-      }
+      },
       {
         //山东泰山
         left: '1300px',
@@ -105,22 +105,129 @@ export default () => [
     ]
   },
   {
-    //天龙
-    id: 2,
-    book_url:
-      'https://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E5%A4%A9%E9%BE%99%E5%85%AB%E9%83%A8.png'
-  },
-  {
-    //射雕
+    //射雕英雄传
     id: 3,
     book_url:
-      'https://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E5%B0%84%E9%9B%95%E8%8B%B1%E9%9B%84%E4%BC%A0.png'
+      'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E5%B0%84%E9%9B%95%E8%8B%B1%E9%9B%84%E4%BC%A0.png',
+    point:
+    [
+      {
+        //江苏
+			  left:'1016px',
+			  top:'766px',
+			  x:true,
+			  desc:'',
+      },
+      {
+        //河北
+			  left:'890px',
+			  top:'597px',
+			  x:false,
+			  desc:'',
+      },
+      {
+        //四川
+        left:'623px',
+        top:'821px',
+        x:false,
+        desc:'',
+      },
+      {
+        //湖北
+        left:'838px',
+        top:'800px',
+        x:false,
+        desc:'',
+      },
+      {
+        //浙江
+        left:'1075px',
+        top:'812px',
+        x:false,
+        desc:'',
+      },
+      {
+        //陕西
+        left:'761px',
+        top:'691px',
+        x:false,
+        desc:'',
+      }
+
+    ]
   },
   {
-    //神雕
-    id: 4,
-    book_url:
-      'https://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E7%A5%9E%E9%9B%95.png'
+    //神雕侠侣
+    id:4,
+    book_url:'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E7%A5%9E%E9%9B%95.png',
+    point:
+    [
+      {
+        //浙江
+        left:'1040px',
+        top:'836px',
+        x:true,
+        desc:'',
+      },
+      {
+        //河南
+        left:'869px',
+        top:'723px',
+        x:false,
+        desc:'',
+      },
+      {
+        //陕西
+        left:'1040px',
+        top:'836px',
+        x:false,
+        desc:'',
+      }
+    ]
   },
+  {
+    //倚天屠龙记
+    id:5,
+    book_url:'http://jinyong-memory.oss-cn-shanghai.aliyuncs.com/subpagemap/page1/%E5%80%9A%E5%A4%A9.png',	
+    point:
+      [
+        {
+          //江苏
+          left:'1010px',
+          top:'723px',
+          x:false,
+          desc:'',
+        },
+        {
+          //甘肃
+          left:'671px',
+          top:'672px',
+          x:false,
+          desc:'',
+        },
+        {
+          //吐蕃
+          left:'233px',
+          top:'580px',
+          x:false,
+          desc:'',
+        },
+        {
+          //四川
+          left:'623px',
+          top:'821px',
+          x:false,
+          desc:'',
+        },
+        {
+          //湖北
+          left:'842px',
+          top:'807px',
+          x:false,
+          desc:'',
+        }
+      ]
+    },
+    
  
 ]
